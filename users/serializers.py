3cbed96483c065a4bf17bd7ad9372e4489bc9e32
Phillip Jhon Lumod <philliplumod@gmail.com>
2025-05-15 13:25:40 +0800

    Add password field to UserSerializer and implement user creation logic

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,16 @@
 from django.contrib.auth.models import User
-
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
     class Meta:
         model = User
-        fields = ['url', 'id', 'username', 'email', 'first_name', 'last_name']
+        fields = ['url', 'id', 'username', 'email', 'first_name', 'last_name', 'password']
